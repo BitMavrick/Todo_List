9a20f5d5845e5ba3bf19b01816a31c0150aaee0a
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-04-01 22:08:09 +0600

    Making cheked Items

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.todolist
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.todolist.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TaskItemClickListener
+{
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var taskViewModel: TaskViewModel
@@ -18,6 +20,30 @@ class MainActivity : AppCompatActivity() {
         binding.newTaskButton.setOnClickListener{
             NewTaskSheet(null).show(supportFragmentManager, "NewTaskTag")
         }
+
+        setRecyclerView()
+
+    }
+
+    private fun setRecyclerView(){
+        val mainActivity = this
+        taskViewModel.taskItems.observe(this){
+            binding.todoListRecyclerView.apply {
+                layoutManager = LinearLayoutManager(applicationContext)
+                adapter = TaskItemAdapter(it, mainActivity)
+
+            }
+        }
+
+
+    }
+
+    override fun editTaskItem(taskItem: TaskItem) {
+        NewTaskSheet(taskItem).show(supportFragmentManager, "newTaskTag")
+    }
+
+    override fun completeTaskItem(taskItem: TaskItem) {
+        taskViewModel.setCompleted(taskItem)
     }
 }
 
--- app/src/main/java/com/example/todolist/TaskItem.kt
+++ app/src/main/java/com/example/todolist/TaskItem.kt
@@ -1,5 +1,7 @@
 package com.example.todolist
 
+import android.content.Context
+import androidx.core.content.ContextCompat
 import java.time.LocalDate
 import java.time.LocalTime
 import java.util.UUID
@@ -11,4 +13,11 @@ class TaskItem(
     var completeDate: LocalDate?,
     var id: UUID = UUID.randomUUID()
 ) {
+    fun isCompleted() = completeDate != null
+    fun imageResource(): Int = if(isCompleted()) R.drawable.checked_24 else R.drawable.unchecked_24
+    fun imageColor(context: Context):Int = if(isCompleted()) purple(context) else black(context)
+
+    private fun purple(context: Context) = ContextCompat.getColor(context, R.color.purple_500)
+    private fun black(context: Context) = ContextCompat.getColor(context, R.color.black)
+
 }
